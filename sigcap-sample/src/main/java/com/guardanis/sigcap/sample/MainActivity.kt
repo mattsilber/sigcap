59cc5a09320b8ad583a7a5a9a606d774a9dadabc
Yordan P.Dieguez <ypdieguez@gmail.com>
2020-03-17 17:22:39 -0400

    Upgraded project to recent versions of android, libraries, etc. Added icon launcher.

--- sigcap-sample/src/main/java/com/guardanis/sigcap/sample/MainActivity.kt
+++ sigcap-sample/src/main/java/com/guardanis/sigcap/sample/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.guardanis.sigcap.sample
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.AppCompatImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageView
 import android.view.View
 import android.widget.Toast
 import com.guardanis.sigcap.NoSignatureException
